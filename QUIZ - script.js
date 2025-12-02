7abca0642842905d92694511dd316bae44ee51b5
maisa-okada <maisa.okada.martins@escola.pr.gov.br>
2025-12-02 12:06:15 -0300

    Update QUIZ - script.js

--- QUIZ - script.js
+++ QUIZ - script.js
@@ -47,7 +47,34 @@ let score = 0;
 function startQuiz() {
   currentQuestionIndex = 0;
   score = 0;
-  nextButton.inner
+  nextButton.innerHTML = "Próxima";
+  showQuestion();
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild)
+     answerButtons.removeChild(answerButtons.firstChild);
+}
+}
+
+function showQuestion() {
+  resetState(
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn);
+    answerButtons.appendChild(button);
+    
+  })
+}
+
+startQuiz();
+
 }
 
 
